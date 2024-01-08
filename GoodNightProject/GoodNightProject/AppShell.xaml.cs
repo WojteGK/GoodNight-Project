8ef85c820378b33399b1c6571320eefe7a553367
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-01-08 14:48:43 +0100

    removed unused options

--- GoodNightProject/GoodNightProject/AppShell.xaml.cs
+++ GoodNightProject/GoodNightProject/AppShell.xaml.cs
@@ -10,9 +10,7 @@ namespace GoodNightProject
     {
         public AppShell()
         {
-            InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            InitializeComponent();            
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
